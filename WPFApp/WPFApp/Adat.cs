80722f3ba6a760388d4bd92f38fd669958ed74e2
pejkob <112940859+pejkob@users.noreply.github.com>
2023-10-02 09:42:42 +0200

    'Új' gomb elkészítve

---
+++ WPFApp/WPFApp/Adat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApp
+{
+    public class Adat
+    {
+        string szemId;
+        string aruAtvevo;
+
+        public string SzemId { get { return szemId; } set { szemId = value; } }
+        public string AruAtvevo { get {  return aruAtvevo; } set {  aruAtvevo = value; } }
+
+        public Adat(string szemId, string aruAtvevo)
+        {
+            SzemId = szemId;
+            AruAtvevo = aruAtvevo;
+        }
+    }
+}
---
+++ WPFApp/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public List<Adat> adatok = new List<Adat>();
+        public static string connectionString = "Server=localhost;Database=aru;User=root;Password=;SSLmode=NONE";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DatabaseConnection(connectionString);
+            AdatokBeolvasasa("customers");
+            dtg_eredmenyek.ItemsSource = adatok;
+        }
+
+        private void DatabaseConnection(string connectionString)
+        {
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    con.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AdatokBeolvasasa(string tableName)
+        {
+            string sqlCommand = $"SELECT * FROM {tableName}";
+
+            try
+            {
+                using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+                {
+                    mySqlConnection.Open();
+
+                    if (tableName == "customers")
+                    {
+                        adatok.Clear();
+                        using (MySqlCommand mySqlCommand = new MySqlCommand(sqlCommand, mySqlConnection))
+                        using (MySqlDataReader myReader = mySqlCommand.ExecuteReader())
+                        {
+                            while (myReader.Read())
+                            {
+                                Adat egyAdat = new Adat(myReader.GetString(0), myReader.GetString(1));
+                                adatok.Add(egyAdat);
+                            }
+                        }
+                    }
+
+                    mySqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_modosit_Click(object sender, RoutedEventArgs e)
+        {
+            string aruatvevo = txb_aruatvevo.Text;
+            string szemid = txb_szemid.Text;
+
+            string sqlCommand = $"UPDATE `customers` SET `Árúátvevő`=@aruatvevo WHERE SzemID=@szemid";
+            try
+            {
+                using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+                {
+                    mySqlConnection.Open();
+
+                    using (MySqlCommand mySqlCommand = new MySqlCommand(sqlCommand, mySqlConnection))
+                    {
+                        mySqlCommand.Parameters.AddWithValue("@aruatvevo", aruatvevo);
+                        mySqlCommand.Parameters.AddWithValue("@szemid", szemid);
+
+                        mySqlCommand.ExecuteNonQuery();
+                    }
+
+                    mySqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Siker");
+
+        }
+
+        private void btn_torol_Click(object sender, RoutedEventArgs e)
+        {
+            string szemid = txb_szemid.Text;
+
+            string sqlCommand = $"DELETE FROM `customers` WHERE SzemID=@szemid";
+            try
+            {
+                using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+                {
+                    mySqlConnection.Open();
+
+                    using (MySqlCommand mySqlCommand = new MySqlCommand(sqlCommand, mySqlConnection))
+                    {
+                        mySqlCommand.Parameters.AddWithValue("@szemid", szemid);
+
+                        mySqlCommand.ExecuteNonQuery();
+                    }
+
+                    mySqlConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Siker");
+
+        }
+
+        private void btn_uj_Click(object sender, RoutedEventArgs e)
+        {
+            string aruatvevo = txb_aruatvevo.Text;
+            string szemid = txb_szemid.Text;
+
+            string sqlCommand = "INSERT INTO `customers` (`SzemID`, `Árúátvevő`) VALUES (@szemid, @aruatvevo)";
+            try
+            {
+                using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+                {
+                    mySqlConnection.Open();
+
+                    using (MySqlCommand mySqlCommand = new MySqlCommand(sqlCommand, mySqlConnection))
+                    {
+                        mySqlCommand.Parameters.AddWithValue("@szemid", szemid);
+                        mySqlCommand.Parameters.AddWithValue("@aruatvevo", aruatvevo);
+
+                        mySqlCommand.ExecuteNonQuery();
+                    }
+
+                    mySqlConnection.Close();
+                }
+                MessageBox.Show("Siker");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
